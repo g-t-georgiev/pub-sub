86d6fd2fda192aff74b11b0d1c21b821be01875f
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-04-16 19:29:55 +0300

    Added test suits.

--- src/test/index.spec.js
+++ src/test/index.spec.js
@@ -1,19 +1,60 @@
-import { PubSub } from '@g.t.georgiev/pubsub';
+import { describe, it, before, after } from 'node:test';
+import assert from 'node:assert';
 
-const eventEmitter = new PubSub();
+import { PubSub } from '../PubSub.js';
 
-const subscription1 = eventEmitter.subscribe('test', (data) => {
-    console.log('Subscriber1: ' + data);
+describe('Create instances', () => {
+
+    it('Should be instantiated as a regular class', () => {
+        assert.ok(new PubSub() instanceof PubSub, 'Not instance of PubSub class.');
+    });
+
+    it('Should be instantiated with the static method.', () => {
+        assert.ok(PubSub.createInstance() instanceof PubSub, 'Not instance of PubSub class.');
+    });
+
 });
 
-const subscription2 = eventEmitter.subscribe('test', (data) => {
-    console.log('Subscriber2: ' + data);
+describe('Subscribe/Publish/Unsubscribe', () => {
+    let eventBus;
+    let values;
+    let subscription;
+
+    before(() => {
+        console.log('Test initial setup');
+        eventBus = new PubSub();
+        values = [ 1, 53, 'Hello', { name: 'John' } ];
+    });
+
+    it('Should subscribe to event', () => {
+        assert.doesNotThrow(() => {
+            subscription = eventBus.subscribe('test', (data) => {
+                assert.ok(values.some(item => Object.is(item, data)), `Expected ${data} to match current list of possible values.`);
+            })
+        });
+    });
+
+    it('Should emit event', () => {
+        assert.doesNotThrow(() => {
+            eventBus.publish('test', 1);
+        });
+
+        assert.doesNotThrow(() => {
+            eventBus.publish('test', 'Hello');
+        });
+
+        assert.doesNotThrow(() => {
+            eventBus.publish('test', values[3]);
+        });
+    });
+
+    it('Should unsubscribe', () => {
+        const unsubscribe = subscription.unsubscribe;
+        assert.ok(typeof unsubscribe === 'function');
+
+        assert.doesNotThrow(() => {
+            unsubscribe();
+        });
+    });
+
 });
-
-const subscription3 = eventEmitter.subscribe('test', (data) => {
-    console.log('Subscriber3: ' + data);
-});
-
-subscription2.unsubscribe();
-
-eventEmitter.publish('test', 'Hello, World!');
